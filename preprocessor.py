484eccfb01f2df4026f116592e0c861e9c2445d6
StarBlazer77 <23tec2cs187@vgu.ac.in>
2025-07-03 21:38:04 +0530

    Create preprocessor.py

---
+++ preprocessor.py
@@ -0,0 +1,51 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s-\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user-message': messages, 'message_date': dates})
+
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+
+    for message in df['user-message']:
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if len(entry) > 2:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user-message'], inplace=True)
+
+    # Feature extraction
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df['day_name'] = df['date'].dt.day_name()
+
+    # Add period column
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append(f"{hour}_00")
+        elif hour == 0:
+            period.append("00_1")
+        else:
+            period.append(f"{hour}_{hour+1}")
+    df['period'] = period
+
+    return df
